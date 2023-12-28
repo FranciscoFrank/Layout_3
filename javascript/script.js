e02d01af6911c8dcb51852a950cfb6bdff0fa28c
Francisco <kolia.boychak@gmail.com>
2023-12-28 17:55:10 +0200

    Cleaned up CSS code and fixed some bugs

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const burgerIcon = document.querySelector('.menu-line');
+    const burgerMenu = document.querySelector('.burger-menu');
+
+    burgerIcon.addEventListener('click', function () {
+        toggleBurgerMenu();
+    });
+
+    document.addEventListener('click', function (event) {
+        const isClickInsideBurgerMenu = burgerMenu.contains(event.target);
+        const isClickInsideBurgerIcon = burgerIcon.contains(event.target);
+
+        if (!isClickInsideBurgerMenu && !isClickInsideBurgerIcon) {
+            closeBurgerMenu();
+        }
+    });
+
+    function toggleBurgerMenu() {
+        burgerMenu.style.display = (burgerMenu.style.display === 'flex') ? 'none' : 'flex';
+    }
+
+    function closeBurgerMenu() {
+        burgerMenu.style.display = 'none';
+    }
+});
